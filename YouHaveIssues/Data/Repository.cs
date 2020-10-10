b7044b2849a80b5265aeb3a37a218260f1e19a11
Kevin Pilch <me@pilchie.com>
2020-10-10 15:23:34 -0700

    Replace cards with Radzen Grid

--- YouHaveIssues/Data/Repository.cs
+++ YouHaveIssues/Data/Repository.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace YouHaveIssues.Data
 {
-    public class Repository
+    public class Repository : IEnumerable<Area>
     {
         private readonly Lazy<IEnumerable<string>> allAreas;
 
@@ -38,5 +39,63 @@ namespace YouHaveIssues.Data
             Milestones = Array.Empty<Milestone>();
             allAreas = new Lazy<IEnumerable<string>>(() => Enumerable.Empty<string>());
         }
+
+        public IEnumerator<Area> GetEnumerator()
+        {
+            var allArea = new Area("All");
+            foreach (var m in Milestones)
+            {
+                allArea.Queries.Add(new Query(m.Name, m.TotalCount));
+            }
+
+            yield return allArea;
+
+            foreach (var areaName in AllAreas)
+            {
+                var area = GetQueriesForArea(areaName);
+                yield return area;
+            }
+        }
+
+        private Area GetQueriesForArea(string areaName)
+        {
+            var area = new Area(areaName);
+            foreach (var m in Milestones)
+            {
+                if (!m.Areas.TryGetValue(areaName, out var query))
+                {
+                    query = new Query(m.Name, 0);
+                }
+                else
+                {
+                    query = new Query(m.Name, query.Count);
+                }
+
+                area.Queries.Add(query);
+            }
+
+            return area;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class Area
+    {
+        public Area(string name)
+            => this.Name = name;
+
+        public string Name { get; }
+
+        public IList<Query> Queries { get; } = new List<Query>();
+
+        public Query this[string milestoneName]
+            => Queries.Single(q => q.Name == milestoneName);
+
+        public Query Get(string milestoneName)
+            => this[milestoneName];
     }
 }
